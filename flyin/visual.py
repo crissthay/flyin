6198dd3f3fb68f149e095301e9dc35994b25eb14
Liza Melo Carrion <liza@MacBook-Pro-de-Liza.local>
2026-08-07 14:55:24 +0100

    done

---
+++ flyin/visual.py
@@ -0,0 +1,21 @@
+class Visual:
+    def __init__(self):
+        pass
+
+    def load_images(self):
+        pass
+
+    def draw_background(self):
+        pass
+
+    def draw_connections(self):
+        pass
+
+    def draw_hubs(self):
+        pass
+
+    def draw_drones(self):
+        ...
+
+    def update(self):
+        ...
