25bfee379f5475646110750ea048f1cc4b7edaa6
Liza Melo Carrion <liza@mbp-de-liza.home>
2026-08-14 16:41:45 +0100

    olha...

--- flyin/drone.py
+++ flyin/drone.py
@@ -2,6 +2,8 @@ class Drone:
     def __init__(self, id_name: str, location) -> None:
         self.id_name: str = id_name
         self.location = location
+        self.in_transit = False
+        self.target = None
+        self.remaining_turns = 0
+        self.connection = None
         #self.indice = indice
-    def __repr__(self):
-        return f"Name({self.id_name}, location{self.location})"
--- flyin/simulation.py
+++ flyin/simulation.py
@@ -40,33 +40,48 @@ class Simulation:
 
 
     def dijkstra(self):
-        distances = {hub: float('inf') for hub in self.hubs}
-        distances[self.start_hub] = 0
+        distances = {
+            hub: (float('inf'), 0)
+            for hub in self.hubs
+        }
+
+        distances[self.start_hub] = (0, 0)
         parent = {
             self.start_hub: None
         }
 
-        queue = [(0, self.start_hub)]
+        queue = [(0, 0, self.start_hub)]
         while queue:
-            current_dis, current_hub = heapq.heappop(queue)
-            if current_dis > distances[current_hub]:
+            current_dis, current_priority, current_hub = heapq.heappop(queue)
+            if current_dis > distances[current_hub][0]:
                 continue
 
             for neighbor in self.get_neighbors(current_hub):
                 if neighbor.is_blocked():
                     continue
-
+                    
                 if neighbor.zone == "restricted":
                     weight = 2
                 else:
                     weight = 1
                 
+                priority = current_priority
+
+                if neighbor.zone == "priority":
+                    priority += 1
                 distance = current_dis + weight
 
-                if distance < distances[neighbor]:
-                    distances[neighbor] = distance
-                    parent[neighbor] = current_hub
-                    heapq.heappush(queue, (distance, neighbor))
+                if (
+                    distance < distances[neighbor][0]
+                    or
+                    (
+                        distance == distances[neighbor][0]
+                        and priority > distances[neighbor][1]
+                    )
+                ):
+                        distances[neighbor] = distance
+                        parent[neighbor] = current_hub
+                        heapq.heappush(queue, (distance, neighbor))
         
         path = []
         if self.end_hub not in parent:
@@ -79,41 +94,6 @@ class Simulation:
 
         path.reverse() 
         return path
-
-
-
-    """ def bfs(self):
-        visited = set()
-        queue = deque([self.start_hub])
-        visited.add(self.start_hub)
-        parent = {
-            self.start_hub: None
-            }
-
-        while queue:
-            hub_now = queue.popleft()
-            if hub_now == self.end_hub:
-                break
-            for hub in self.get_neighbors(hub_now):
-                if hub.is_blocked():
-                    continue
-                if hub in visited:
-                    continue
-
-                visited.add(hub)
-                queue.append(hub)
-                parent[hub] = hub_now
-        path = []
-        current = self.end_hub
-        
-        if self.end_hub not in parent:
-            return []
-        while current is not None:
-            path.append(current)
-            current = parent[current]
-
-        path.reverse() 
-        return path"""
     
     def check_connect(self, hub1, hub2):
         for connect in self.connections:
@@ -143,40 +123,6 @@ class Simulation:
             return True
         return False
 
-    """def simulate(self):
-        path = self.dijkstra()
-        print("PATH:", [hub.name for hub in path])
-        if not path:
-            print("Can't move: no path to destination.")
-            return
-        turn = 1
-        while any(drone.location != self.end_hub for drone in self.drone_list):
-            print(f"\nTurn {turn}")
-            moved = False
-
-            for drone in self.drone_list:
-                if drone.location == self.end_hub:
-                    continue
-
-                indice = path.index(drone.location)
-                if indice == len(path) - 1:
-                    continue
-
-                next_hub = path[indice + 1]
-                if self.move_drone(drone, next_hub):
-                    moved = True
-                    print(drone.id_name, "->", next_hub.name)
-
-            for connection in self.connections:
-                connection.drones.clear()  
-
-            turn += 1
-
-            if not moved:
-                print("Can't move anymore.")
-                break
-"""
-
     def simulate(self):
         path = self.dijkstra()
         turn = 1
@@ -190,14 +136,37 @@ class Simulation:
             for drone in self.drone_list
         ):
             print(f"\nTURN {turn}")
-            moves = []
-            moving_out = set()
 
             for drone in self.drone_list:
+                if not drone.in_transit:
+                    continue
+
+                drone.remaining_turns -= 1
+
+                if drone.remaining_turns == 0:
+                    drone.target.add_drone(drone)
+                    drone.location = drone.target
+
+                    drone.in_transit = False
+                    drone.target = None
+                    drone.connection = None
+
+                    print(
+                        drone.id_name,
+                        "arrived at",
+                        drone.location.name
+                    )
+
+            moves = []
+
+            for drone in self.drone_list:
+
                 if drone.location == self.end_hub:
                     continue
-                indice = path.index(drone.location)
 
+                if drone.in_transit:
+                    continue
+                indice = path.index(drone.location)
                 if indice == len(path) - 1:
                     continue
 
@@ -210,10 +179,16 @@ class Simulation:
                 moves.append(
                     (drone, next_hub, connection)
                 )
-                moving_out.add(drone.location)
+
             moved = False
+
             for drone, next_hub, connection in moves:
+
+                # blockeddd 
                 if next_hub.is_blocked():
+                    continue
+
+                if connection.is_full_connect():
                     continue
 
                 leaving = sum(
@@ -231,21 +206,36 @@ class Simulation:
                 if available_space <= 0:
                     continue
 
-                if connection.is_full_connect():
-                    continue
-
                 connection.add_drone(drone)
                 drone.location.remove_drone(drone)
-                next_hub.add_drone(drone)
-                drone.location = next_hub
+
+                if next_hub.zone == "restricted":
+
+                    drone.in_transit = True
+                    drone.target = next_hub
+                    drone.remaining_turns = 1
+                    drone.connection = connection
+
+                    print(
+                        drone.id_name,
+                        "->",
+                        next_hub.name,
+                        "(restricted)"
+                    )
+
+                else:
+
+                    next_hub.add_drone(drone)
+                    drone.location = next_hub
+
+                    print(
+                        drone.id_name,
+                        "->",
+                        next_hub.name
+                    )
 
                 moved = True
 
-                print(
-                    drone.id_name,
-                    "->",
-                    next_hub.name
-                )
             for connection in self.connections:
                 connection.drones.clear()
 
